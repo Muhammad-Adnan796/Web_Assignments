aafd3a04756b0f3a6873565121637e6c2f94b11a
Muhammad Adnan <adnan108123796@gmail.com>
2022-10-05 11:24:15 +0500

    Assignment 1

---
+++ app.js
@@ -0,0 +1,327 @@
+
+
+// Alert Is used for to show the out put in pop in alert box
+
+// alert("Hello World");
+
+
+
+// Consol.log is also used for  show the out put of in google console
+
+// console.log("Hello World");
+
+
+
+// Document.write is also used for show the output on the browser screen 
+// document.write("Muhammad Adnan")
+
+
+
+
+//        typeof DataType is used for show the variable type sach as varible is number or String or a boolean value
+
+// var numb = 30;
+
+// var name1 = "Adnan";
+// var isVisible = true; 
+
+// document.write( typeof numb );
+// document.write( typeof name1 );
+// document.write( typeof isVisible );
+
+
+// console.log( typeof numb);
+// console.log( typeof name1 );
+// console.log( typeof isVisible);
+
+
+
+//                           Assignment 1
+
+
+// Q # 1
+document.write("Wellcome to JavaScript ");
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+// Q # 2
+// Bio Data
+
+
+alert("Muhammad Adnan");
+alert(" I am a 21 year old");
+alert("Certified Mobile Application Development");
+
+
+// Q # 3
+
+var email = "adnan8123796@gmail.com";
+alert(" My Email Address is " + email);
+
+
+
+// Q # 4
+
+document.write("Yah! I can write a HTML content through JAVASCRIPT ears old");
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+// Q # 5
+
+var age = 21;
+alert("I am "+ age + " years old");
+
+// Q # 6
+
+var birthYear = "My Birth year is ";
+
+document.write(birthYear + " 2001");
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+// Q # 8
+
+var num1 = 3;
+var num2 = 5;
+
+var numb = num1 + num2;
+
+document.write("Sum of "+ num1 +" and " + num2 + " is " + numb);
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+
+// Q # 9 subtraction
+
+var num1 = 3;
+var num2 = 5;
+
+var numb = num1 - num2;
+
+document.write("Subtraction of "+ num1 +" and " + num2 + " is " + numb);
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+
+// Q # 9 Multiplication
+
+var num1 = 3;
+var num2 = 5;
+
+var numb = num1 * num2;
+
+document.write("Multiplication of "+ num1 +" and " + num2 + " is " + numb);
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+
+// Q # 9 Division
+
+var num1 = 3;
+var num2 = 5;
+
+var numb = num1 / num2;
+
+document.write("Division of "+ num1 +" and " + num2 + " is " + numb);
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+
+// Q # 9 Modulus
+
+var num1 = 10;
+var num2 = 3;
+
+var numb = num1 % num2;
+
+document.write("Modulus of "+ num1 +" and " + num2 + " is " + numb);
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+
+
+// Q # 10 
+// (a)
+
+var undi;
+
+//(b)
+
+document.write("Value after variable declaration is "+ undi);
+document.write("<br>");
+
+// (c)
+
+undi = 5;
+
+// (d)
+
+document.write("Initial value: "+undi);
+document.write("<br>");
+
+// (e)
+
+undi++;
+
+// (f)
+
+document.write("Value after increment is: "+undi);
+document.write("<br>");
+
+// (g)
+
+var addition = undi + 7;
+
+// (h)
+
+document.write("Value after addition is: "+addition);
+document.write("<br>");
+
+// (i)
+
+addition--;
+
+// (j)
+
+document.write("Value after decrement is: "+addition);
+document.write("<br>");
+
+// (k)
+
+var reminder = addition % 3;
+
+document.write("The reminder is: "+reminder);
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+// Q # 11
+
+// (a)
+
+var celciusTemp = 25;
+
+// (b)
+
+var converting_fahren = (  celciusTemp * 9 / 5  ) + 32;
+document.write( celciusTemp  + "\u00B0C is   " + converting_fahren + "\u00B0F" );
+document.write("<br>");
+
+// (c)
+
+var fahrenTemp = 70;
+
+var converting_celcius = ( fahrenTemp - 32 ) * 5 / 9 ;
+
+document.write( fahrenTemp  + "\u00B0F is   " + converting_celcius + "\u00B0C" );
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+
+
+// Q # 12 
+
+var dolllars = 10 * 155;
+var saudi_riyals = 25 * 41;
+var total = dolllars + saudi_riyals;
+document.write("Curruncy in PKR");
+document.write("<br>");
+document.write("<br>");
+
+document.write("Total currency in PKR: "+ total);
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+
+// Q # 13
+
+var a = 10;
+
+document.write("Now the value of a is: " + a);
+document.write("<br>");
+document.write("<br>");
+document.write("...............");
+document.write("<br>");
+document.write("<br>");
+
+
+//              Pre Incerement
+document.write("The value of ++a is: " + ++a);
+document.write("<br>");
+document.write("Now the value of a is: " + a);
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+
+//              Post Increment
+document.write("The value of a++ is: " + a++);
+document.write("<br>");
+document.write("Now the value of a is: " + a);
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+//              Pre Decrement
+document.write("The value of --a is: " + --a);
+document.write("<br>");
+document.write("Now the value of a is: " + a);
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+
+//              Post Decrement
+document.write("The value of a-- is: " + a--);
+document.write("<br>");
+document.write("Now the value of a is: " + a);
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+
+
+//  Q # 14 
+
+
+var a1 = 2;
+var b1 = 1;
+
+--a1;
+document.write(" --a is : " + a1); // 1
+document.write("<br>");
+
+var result1 = --a1 - --b1;
+document.write(" --a1 - --b1 is : " + result1); // 0
+document.write("<br>");
+
+var result2 = --a1 - --b1 + ++b1;
+document.write(" --a1 - --b1 + ++b1 is : " + result2); // 0
+document.write("<br>");
+
+var result = --a1 - --b1 + ++b1 + b1--;
+document.write(" --a1 - --b1 + ++b1 + b1-- is : " + result); // -1
+document.write("<br>");
+document.write("<br>");
+
+
+
+
+
+
+
+
+
+
+
+
+
