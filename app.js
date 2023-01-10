02c5a10d31794ebf4a446b1a2f99f72a56c617e8
Muhammad Adnan <adnan108123796@gmail.com>
2023-01-11 00:35:29 +0500

    chapter-2-assi-js-rj

---
+++ app.js
@@ -0,0 +1,112 @@
+
+
+//                    Chapter # 2 (Assignment);
+document.write("<br><br><br>");
+document.write("<b>Chapter # 2 (Assignment)</b> <br><br><br><br>");
+
+//  1. Declare a variable called username.
+
+// Answer - 1
+
+document.write(`Answer -1 <br>`);
+var username ;
+username = "Muhammad Adnan";
+document.write(`${username} <br><br><br>`);
+
+
+//  2. Declare a variable called myName & assign to it a string
+//  that represents your Full Name.
+
+// Answer - 2
+document.write(`Answer -2 <br>`);
+var myName ;
+myName = "Muhammad Adnan";
+document.write(`Full Name: <b>${myName}</b> <br><br><br>`);
+
+
+//  3. Write script to
+//  a) Declare a JS variable, titled message.
+//  b) Assign “Hello World” to variable message
+//  c) Display the message in alert box.
+
+
+ // Answer - 3
+ document.write(`Answer -3 <br>`);
+ var message;
+ message = "Hello World";
+ alert(message);
+ document.write(`${message} <br><br><br>`);
+
+
+//  4. Write a script to save student’s bio data in JS variables and
+ //  show the data in alert boxes.
+
+
+ // Answer - 4
+ document.write(`Answer -4 <br>`);
+ var name1 = "Muhammad Adnan";
+ var age = 21;
+ var certificate = "Certified Mobile Application Development";
+ alert(`Name : ${name1}`) ;
+ alert(`Age : ${age}`) ;
+ alert(`Certificate : ${certificate}`);
+ document.write(`${message}, <br> ${age}, <br> ${certificate}, <br><br><br>`);
+
+
+//  5. Write a script to display the following alert using one JS
+// variable:
+
+
+// Answer-5
+document.write(`Answer -5 <br>`);
+alert("PIZZA \nPIZZ \nPIZ \nPI \nP ") ;
+document.write("PIZZA <br> PIZZ <br> PIZ <br> PI <br> P <br><br><br> ");
+
+
+// 6. Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation)
+
+
+// Answer-6
+document.write(`Answer -6 <br>`);
+var email ;
+email = "adnan108123796@gmail.com";
+alert(`My email address is : ${email}`);
+document.write(`My email address is : <a href="">${email}<a> <br><br><br>`);
+
+
+// 7. Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+
+
+// Answer-7
+document.write(`Answer -7 <br>`);
+var book ;
+book = "A smarter way to learn JavaScript";
+alert(`I am trying to learn from the book "${book}"`);
+document.write(`I am trying to learn from the book "<b>${book}</b>" <br><br><br>`);
+
+
+// 8. Write a script to display this in browser through JS
+
+// Answer-8
+document.write(`Answer -8 <br>`);
+document.write(`Yah! I can write <b>HTML</b> content through Javascript <br><br><br>`);
+
+
+// 9. Store following string in a variable and show in alert and
+// browser through JS
+// “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”
+
+
+// Answer-9
+document.write(`Answer -9 <br>`);
+alert(`▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬`);
+document.write(`▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬ <br><br><br>`);
+
+                                // END....Assignemnt-1
+
+                                
