e4674d215aee75dd1b15d09512eb7d52e7e39f3f
Muhammad Adnan <adnan108123796@gmail.com>
2023-01-11 00:40:39 +0500

    chapter-3-assi-js-rj

---
+++ app.js
@@ -0,0 +1,64 @@
+
+//                    Chapter # 3 (Assignment);
+document.write("<br><br><br>");
+document.write("<b>Chapter # 3 (Assignment)</b> <br><br><br><br>");
+
+
+// 1.
+//  Declare a variable called age & assign to it your age. Show
+// your age in an alert box.
+
+// Answer -1
+document.write(`Answer -1 <br>`);
+var age ;
+age = 21;
+alert(`I am ${age} years old `);
+document.write(`I am ${age} years old <br><br><br>`)
+
+
+// 2.
+//  Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You
+// have visited this site N times”.
+
+// Answer -2
+document.write(`Answer -2 <br>`);
+
+var visitTimes ;
+visitTimes = 14;
+alert(`You have visited this site ${visitTimes} times`);
+document.write(`You have visited this site ${visitTimes} times <br><br><br> `);
+
+// 3. Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browser:
+
+// Answer-3
+document.write(`Answer -3 <br>`);
+var birthYear ;
+birthYear = 22;
+
+document.write(`My birth year is <b>${birthYear}</b> <br>`);
+document.write(`Data type of my declared variable is ${typeof birthYear} <br><br><br>`);
+
+
+// 4. A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+
+// Answer-4
+document.write(`Answer -4 <br>`);
+var visitorName = "Muhammad Adnan";
+var quantity = 5;
+var product = "T Shirt(s)";
+var storeName = "Daraz";
+document.write(`<br> ${visitorName} ordered ${quantity} ${product} on ${storeName} Clothing Store. <br><br><br>`);
+
+
+                        // END Chapter -3...
