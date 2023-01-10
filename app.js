29d7109285dc4b73c298863d1d298076036b31d4
Muhammad Adnan <adnan108123796@gmail.com>
2023-01-11 00:31:27 +0500

    chapter-1-assi-js-rj

---
+++ app.js
@@ -0,0 +1,72 @@
+
+//            Chapter # 1 (Assignment)
+document.write("<br><br><br>");
+document.write("<b>Chapter # 1 (Assignment)</b> <br><br><br><br>");
+
+
+// 1. Write a script to greet your website visitor using JS alert
+// box.
+
+// Anwer-1
+document.write("Answer -1 <br>");
+alert("WellCome to my first web!");
+document.write("WellCome to my first web! <br><br><br>");
+
+// Write a script to display following message on your web
+// page:
+
+
+// Answer-2
+document.write("Answer -2 <br>");
+alert("Error! Please enter a valid password.");
+document.write("Error! Please enter a valid password.<br><br><br>");
+
+// 3. Write a script to display following message on your web
+// page: (Hint : Use line break)
+
+
+// Answer-3
+document.write("Answer -3 <br>");
+alert("Welcome to JS Land...\nHappy Coding!");
+document.write("Welcome to JS Land...\nHappy Coding! <br><br><br>");
+
+// 4. Write a script to display following messages in sequence:
+
+// Answer-4
+document.write("Answer -4 <br>");
+alert("Welcome to JS Land...");
+alert("Happy coding! \nPrevent this page from creating additional dialogs");
+document.write("Welcome to JS Land...\nHappy coding! \nPrevent this page from creating additional dialogs <br><br><br>");
+
+
+//5. Generate the following message through browser’s
+// developer console:
+
+
+// Asnwer-5
+document.write("Answer -5 <br>");
+console.log("Hello...I can run JS through my web browser's console");
+document.write("Hello...I can run JS through my web browser's console <br><br><br>");
+
+
+// 6. Make use of alerts in your new/existing HTML & CSS
+// project.
+
+// Answer-6
+document.write("Answer -6 <br>");
+
+
+// 7. Practice placement of <script></script> element in
+// following sections of your project in exercise 6:
+// a. Head
+// b. Body (before your page’s HTML)
+// ALERTS | JAVASCRIPT
+// Page 3 of 3
+// c. Body (inside your page’s HTML)
+// d. Body (after your page’s HTML)
+
+// Answer-7
+document.write("Answer -7 <br>");
+
+                        // END....Assignemnt-1
+
